fe3e8807c5a323c00faebd668f7b559a3583cec2
jvandaal <jonas.vandaal@outlook.be>
2024-03-07 10:35:51 +0100

    feat: move to dotnet 8.0.2
    move to dotnet 8.0.2
    
    BREAKING CHANGE: move to dotnet 8.0.2

--- src/Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Middleware.AddCorrelationIdToResponse/Properties/AssemblyInfo.cs
+++ src/Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Middleware.AddCorrelationIdToResponse/Properties/AssemblyInfo.cs
@@ -5,8 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyProduct("Vlaamse basisregisters")]
 [assembly: AssemblyDescription("ASP.NET Core MVC Middleware to add an 'x-correlation-id' header to all responses.")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
 
